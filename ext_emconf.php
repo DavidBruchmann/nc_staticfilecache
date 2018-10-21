afd26a696a0a1d3f3cabf8dcb116def57233be04
DavidBruchmann <david.bruchmann@gmail.com>
2018-10-21 21:59:27 +0700

    frontend user login detection with own cookie updated manual no more negative cache headers cache headers send on access just like with TYPO3 cache removal of cleaner script (not needed any longer because of new header code) added TODO to manual

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "nc_staticfilecache".
 #
-# Auto generated 12-08-2015 21:27
+# Auto generated 09-08-2015 11:14
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,14 +15,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Transparent static file cache solution using mod_rewrite and mod_expires. Increase response times for static pages by a factor of 230!',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'TYPO3_version' => '',
 	'PHP_version' => '',
-	'module' => 'cli/pre_4.1',
+	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
